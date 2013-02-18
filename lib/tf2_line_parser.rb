578bbe8f26f23415381102c7683c53393f71a69d
Arie <github@ariekanarie.nl>
2013-02-19 00:16:44 +0100

    unused monkey patch removed

--- lib/tf2_line_parser.rb
+++ lib/tf2_line_parser.rb
@@ -1,7 +1,7 @@
-require "ext/string"
 require "tf2_line_parser/version"
 require "tf2_line_parser/parser"
 require "tf2_line_parser/player"
+
 require "tf2_line_parser/events/event"
 require "tf2_line_parser/events/score"
 Dir[File.dirname(__FILE__) + '/tf2_line_parser/events/*.rb'].each {|file| require file }
