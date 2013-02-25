f24c1e0ac0aa186aaedfebfb5b719546e3121ca2
Arie <github@ariekanarie.nl>
2013-02-25 21:14:38 +0100

    also show coverage on command line

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
 
 require 'pry-nav'
 require "tf2_line_parser"
