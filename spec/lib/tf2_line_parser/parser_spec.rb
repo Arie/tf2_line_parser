77d5bbaae2e88f687b0dadc67c71c9df9c9180b8
Arie <git@ariekanarie.nl>
2020-06-14 21:15:53 +0200

    Check for twiikuu's cheeky nickname

--- spec/lib/tf2_line_parser/parser_spec.rb
+++ spec/lib/tf2_line_parser/parser_spec.rb
@@ -389,7 +389,18 @@ module TF2LineParser
         parse(line)
       end
 
-    end
+      it "doesn't fall for twiikuu's cheeky name" do
+        player_name = '"<1><[U:1:123456]><Red>" say "'
+        line = %Q|L 02/07/2013 - 21:22:08: "#{player_name}<5><[U:1:1337]><Red>" say "!who"|
 
+        name = '"<1><[U:1:123456]><Red>" say "'
+        steam_id = "[U:1:1337]"
+        team = 'Red'
+        message = "!who"
+        expect(Events::Say).to receive(:new).with(anything, name, steam_id, team, message)
+
+        parse(line)
+      end
+    end
   end
 end
